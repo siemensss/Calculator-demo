b0563b4f9aeb867f509141f0eb73584484b24c53
siemens <sema.perstnev.01@mail.ru>
2023-03-19 01:38:19 +0300

    ДЗ (Maven, Spring Boot, web), Перстнев С. *fixed*

--- src/main/java/calculator/my/calculatordemo/CalcService.java
+++ src/main/java/calculator/my/calculatordemo/CalcService.java
@@ -2,9 +2,9 @@ package calculator.my.calculatordemo;
 
 public interface CalcService {
     String sayHello ();
-    String sum (String num1, String num2);
-    String sub (String num1, String num2);
-    String mult (String num1, String num2);
-    String div ( String num1,  String num2);
+    int sum (String num1, String num2);
+    int sub (String num1, String num2);
+    int mult (String num1, String num2);
+    int div ( String num1,  String num2);
 
 }
--- src/main/java/calculator/my/calculatordemo/CalcServiceImpl.java
+++ src/main/java/calculator/my/calculatordemo/CalcServiceImpl.java
@@ -10,19 +10,19 @@ public class CalcServiceImpl implements CalcService {
         return "Добро пожаловать в калькулятор";
     }
 
-    public String sum (String num1, String num2) {
-        return String.valueOf((Integer.parseInt(num1) + Integer.parseInt(num2)));
+    public int sum (String num1, String num2) {
+        return Integer.parseInt(num1) + Integer.parseInt(num2);
     }
 
-    public String sub (String num1, String num2) {
-        return String.valueOf((Integer.parseInt(num1) - Integer.parseInt(num2)));
+    public int sub (String num1, String num2) {
+        return Integer.parseInt(num1) - Integer.parseInt(num2);
     }
 
-    public String mult (String num1, String num2) {
-        return String.valueOf((Integer.parseInt(num1) * Integer.parseInt(num2)));
+    public int mult (String num1, String num2) {
+        return Integer.parseInt(num1) * Integer.parseInt(num2);
     }
 
-    public String div ( String num1,  String num2) {
-        return String.valueOf((Integer.parseInt(num1) / Integer.parseInt(num2)));
+    public int div ( String num1,  String num2) {
+        return Integer.parseInt(num1) / Integer.parseInt(num2);
     }
 }
